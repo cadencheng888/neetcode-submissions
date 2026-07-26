2226f9d94bbad9697c676a2f98a1f9b925435410
Caden Cheng <cadencheng888@gmail.com>
2026-07-26 10:30:34 -0700

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,14 @@
+import string
+import math
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.translate(str.maketrans('', '', string.punctuation))
+        s = s.lower()
+        s = s.replace(" ", "")
+        length = len(s)
+        for i in range(math.floor(length / 2)):
+            if s[i] != s[length - i - 1]:
+                return False
+        return True
+
+        
