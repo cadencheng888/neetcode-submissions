015fb8fd4a21eeb4d890029866a256242e489e54
Caden Cheng <cadencheng888@gmail.com>
2026-07-30 22:24:40 -0700

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,13 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        return collections.Counter(s) == collections.Counter(t)
+        for char in s:
+            s_chars[char] += 1
+        for char in t:
+            t_chars[char] += 1
+        return s_chars == t_chars
+        
