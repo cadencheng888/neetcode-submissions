68f5158afa411184b914c4a8bf901707df54c0de
Caden Cheng <cadencheng888@gmail.com>
2026-09-09 23:39:20 -0700

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.right.prev = self.left
+        self.left.next = self.right
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    def insert(self, node):
+        next_node = self.right
+        prev_node = self.right.prev
+        next_node.prev = node
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = next_node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+        
